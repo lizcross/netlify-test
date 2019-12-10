64054ab9ba4adfdc63d21e68a2e2584785fb4946
Liz Cross <elizabeth.cross@gmail.com>
2019-12-10 10:19:01 -0800

    Update with sample markdown app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Remarkable } from 'remarkable';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MarkdownEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = { value: 'Hello, **world**!' };
+    }
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    getRawMarkup() {
+        const md = new Remarkable();
+        return { __html: md.render(this.state.value) };
+    }
+
+    render() {
+        return (
+            <div className="MarkdownEditor">
+                <h3>Input</h3>
+                <label htmlFor="markdown-content">
+                    Enter some markdown
+          </label>
+                <textarea
+                    id="markdown-content"
+                    onChange={this.handleChange}
+                    defaultValue={this.state.value}
+                />
+                <h3>Output</h3>
+                <div
+                    className="content"
+                    dangerouslySetInnerHTML={this.getRawMarkup()}
+                />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <MarkdownEditor />,
+    document.getElementById('root')
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
